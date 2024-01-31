e13d7be2f839d72da7b940d9b68286b4d8886c4c
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-01-31 14:01:43 +0330

    new update js

--- Login/src/script.js
+++ Login/src/script.js
@@ -2,49 +2,55 @@ var themes = [
     {
         background: "#1A1A2E",
         color: "#FFFFFF",
-        primaryColor: "#0F3460"
+        primaryColor: "#0F3460",
     },
     {
         background: "#461220",
         color: "#FFFFFF",
-        primaryColor: "#E94560"
+        primaryColor: "#E94560",
     },
     {
         background: "#192A51",
         color: "#FFFFFF",
-        primaryColor: "#967AA1"
+        primaryColor: "#967AA1",
     },
     {
         background: "#F7B267",
         color: "#000000",
-        primaryColor: "#F4845F"
+        primaryColor: "#F4845F",
     },
     {
         background: "#F25F5C",
         color: "#000000",
-        primaryColor: "#642B36"
+        primaryColor: "#642B36",
     },
     {
         background: "#231F20",
         color: "#FFF",
-        primaryColor: "#BB4430"
-    }
+        primaryColor: "#BB4430",
+    },
 ];
 var setTheme = function (theme) {
     var root = document.querySelector(":root");
-    root.style.setProperty("--background", theme.background);
-    root.style.setProperty("--color", theme.color);
-    root.style.setProperty("--primary-color", theme.primaryColor);
-    root.style.setProperty("--glass-color", theme.glassColor);
+    if (root) {
+        root.style.setProperty("--background", theme.background);
+        root.style.setProperty("--color", theme.color);
+        root.style.setProperty("--primary-color", theme.primaryColor);
+        if (theme.glassColor) {
+            root.style.setProperty("--glass-color", theme.glassColor);
+        }
+    }
 };
 var displayThemeButtons = function () {
     var btnContainer = document.querySelector(".theme-btn-container");
-    themes.forEach(function (theme) {
-        var div = document.createElement("div");
-        div.className = "theme-btn";
-        div.style.cssText = "background: ".concat(theme.background, "; width: 25px; height: 25px");
-        btnContainer.appendChild(div);
-        div.addEventListener("click", function () { return setTheme(theme); });
-    });
+    if (btnContainer) {
+        themes.forEach(function (theme) {
+            var div = document.createElement("div");
+            div.className = "theme-btn";
+            div.style.cssText = "background: ".concat(theme.background, "; width: 25px; height: 25px");
+            btnContainer.appendChild(div);
+            div.addEventListener("click", function () { return setTheme(theme); });
+        });
+    }
 };
 displayThemeButtons();
