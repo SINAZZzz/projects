1334061a82300d02536df1fd2ec711cdbe866a43
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-01-31 14:00:05 +0330

    new update ts

--- Login/src/script.ts
+++ Login/src/script.ts
@@ -1,53 +1,73 @@
-const themes = [
+const themes: Array<{
+    background: string;
+    color: string;
+    primaryColor: string;
+}> = [
     {
         background: "#1A1A2E",
         color: "#FFFFFF",
-        primaryColor: "#0F3460"
+        primaryColor: "#0F3460",
     },
     {
         background: "#461220",
         color: "#FFFFFF",
-        primaryColor: "#E94560"
+        primaryColor: "#E94560",
     },
     {
         background: "#192A51",
         color: "#FFFFFF",
-        primaryColor: "#967AA1"
+        primaryColor: "#967AA1",
     },
     {
         background: "#F7B267",
         color: "#000000",
-        primaryColor: "#F4845F"
+        primaryColor: "#F4845F",
     },
     {
         background: "#F25F5C",
         color: "#000000",
-        primaryColor: "#642B36"
+        primaryColor: "#642B36",
     },
     {
         background: "#231F20",
         color: "#FFF",
-        primaryColor: "#BB4430"
-    }
+        primaryColor: "#BB4430",
+    },
 ];
 
-const setTheme = (theme) => {
-    const root = document.querySelector(":root");
-    root.style.setProperty("--background", theme.background);
-    root.style.setProperty("--color", theme.color);
-    root.style.setProperty("--primary-color", theme.primaryColor);
-    root.style.setProperty("--glass-color", theme.glassColor);
+type Themes =  {
+    background: string;
+    color: string;
+    primaryColor: string;
+    glassColor?: string; 
+}
+
+const setTheme = (theme:Themes): void => {
+    const root = document.querySelector(":root") as HTMLElement | null;
+
+    if (root) {
+        root.style.setProperty("--background", theme.background);
+        root.style.setProperty("--color", theme.color);
+        root.style.setProperty("--primary-color", theme.primaryColor);
+
+        if (theme.glassColor) {
+            root.style.setProperty("--glass-color", theme.glassColor);
+        }
+    }
 };
 
-const displayThemeButtons = () => {
-    const btnContainer = document.querySelector(".theme-btn-container");
-    themes.forEach((theme) => {
-        const div = document.createElement("div");
-        div.className = "theme-btn";
-        div.style.cssText = `background: ${theme.background}; width: 25px; height: 25px`;
-        btnContainer.appendChild(div);
-        div.addEventListener("click", () => setTheme(theme));
-    });
+const displayThemeButtons = (): void => {
+    const btnContainer = document.querySelector(".theme-btn-container") as HTMLElement | null;
+
+    if (btnContainer) {
+        themes.forEach((theme) => {
+            const div = document.createElement("div");
+            div.className = "theme-btn";
+            div.style.cssText = `background: ${theme.background}; width: 25px; height: 25px`;
+            btnContainer.appendChild(div);
+            div.addEventListener("click", () => setTheme(theme));
+        });
+    }
 };
 
 displayThemeButtons();
