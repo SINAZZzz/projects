0f3c0c86b1a04e1cc5c1793e202766e4c723758f
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-02-07 11:00:01 +0330

    complete add task

--- AddTask/public/script.js
+++ AddTask/public/script.js
@@ -1,56 +1,16 @@
 "use strict";
-//     const username = document.getElementById('username') as HTMLInputElement;
-//     const task = document.getElementById('task') as HTMLInputElement;
-//     const usernameValue: string = username.value;
-//     const taskValue: string = task.value;
-// function createElementOnClick(): void {
-//     const newDiv = document.createElement('div');
-//     newDiv.textContent = 'This is a new div';
-//     newDiv.style.color = 'red';
-//     document.body.appendChild(newDiv);
-// }
-// const addButton = document.getElementById('submit');
-// if(addButton){
-//     addButton.addEventListener('click', () => {
-//         if(usernameValue === '' || taskValue === ''){
-//             alert("Error");
-//         }else if () {
-//             createElementOnClick()
-//         }
-//     })
-// }
-// function createElementOnClick(usernameValue: string, taskValue: string): void {
-//     const newDiv = document.createElement('div');
-//     newDiv.textContent = `username :  ${usernameValue} >>> task : ${taskValue} `;
-//     newDiv.style.color = 'red';
-//     document.body.appendChild(newDiv);
-// }
-// const addButton = document.getElementById('submit');
-// if (addButton) {
-//     addButton.addEventListener('click', () => {
-//         const username = document.getElementById('username') as HTMLInputElement;
-//         const task = document.getElementById('task') as HTMLInputElement;
-//         const usernameValue: string = username.value;
-//         const taskValue: string = task.value;
-//         if (usernameValue === '' || taskValue === '') {
-//             alert("Error: Please fill in all fields");
-//         } else {
-//             createElementOnClick(usernameValue, taskValue);
-//         }
-//     });
-// }
-var usernameInput = document.getElementById('username');
-var taskInput = document.getElementById('task');
-function createElementOnClick(usernameValue, taskValue) {
-    var newDiv = document.createElement('div');
+var usernameInput, taskInput, newDiv, createElementOnClick, addButton;
+usernameInput = document.getElementById('username');
+taskInput = document.getElementById('task');
+addButton = document.getElementById('submit');
+createElementOnClick = function (usernameValue, taskValue) {
+    newDiv = document.createElement('div');
     newDiv.textContent = "username :  ".concat(usernameValue, " >>> task : ").concat(taskValue, " ");
     newDiv.style.color = 'red';
     document.body.appendChild(newDiv);
-    // Clear input fields
     usernameInput.value = '';
     taskInput.value = '';
-}
-var addButton = document.getElementById('submit');
+};
 if (addButton) {
     addButton.addEventListener('click', function () {
         var usernameValue = usernameInput.value;
--- AddTask/src/script.ts
+++ AddTask/src/script.ts
@@ -1,77 +1,20 @@
 
-//     const username = document.getElementById('username') as HTMLInputElement;
-//     const task = document.getElementById('task') as HTMLInputElement;
+let usernameInput:HTMLInputElement , taskInput:HTMLInputElement , newDiv:HTMLDivElement , createElementOnClick:Function ,
+ addButton:HTMLElement | null
 
-//     const usernameValue: string = username.value;
-//     const taskValue: string = task.value;
+usernameInput = document.getElementById('username') as HTMLInputElement;
+taskInput = document.getElementById('task') as HTMLInputElement;
+addButton = document.getElementById('submit');
 
-
-
-
-
-
-// function createElementOnClick(): void {
-//     const newDiv = document.createElement('div');
-
-//     newDiv.textContent = 'This is a new div';
-//     newDiv.style.color = 'red';
-
-//     document.body.appendChild(newDiv);
-// }
-
-// const addButton = document.getElementById('submit');
-
-// if(addButton){
-//     addButton.addEventListener('click', () => {
-//         if(usernameValue === '' || taskValue === ''){
-//             alert("Error");
-//         }else if () {
-//             createElementOnClick()
-//         }
-//     })
-// }
-
-// function createElementOnClick(usernameValue: string, taskValue: string): void {
-//     const newDiv = document.createElement('div');
-//     newDiv.textContent = `username :  ${usernameValue} >>> task : ${taskValue} `;
-//     newDiv.style.color = 'red';
-//     document.body.appendChild(newDiv);
-// }
-
-// const addButton = document.getElementById('submit');
-
-// if (addButton) {
-//     addButton.addEventListener('click', () => {
-//         const username = document.getElementById('username') as HTMLInputElement;
-//         const task = document.getElementById('task') as HTMLInputElement;
-
-//         const usernameValue: string = username.value;
-//         const taskValue: string = task.value;
-
-//         if (usernameValue === '' || taskValue === '') {
-//             alert("Error: Please fill in all fields");
-//         } else {
-//             createElementOnClick(usernameValue, taskValue);
-//         }
-//     });
-// }
-
-
-const usernameInput = document.getElementById('username') as HTMLInputElement;
-const taskInput = document.getElementById('task') as HTMLInputElement;
-
-function createElementOnClick(usernameValue: string, taskValue: string): void {
-    const newDiv = document.createElement('div');
+createElementOnClick = (usernameValue: string, taskValue: string): void =>{
+    newDiv = document.createElement('div');
     newDiv.textContent = `username :  ${usernameValue} >>> task : ${taskValue} `;
     newDiv.style.color = 'red';
     document.body.appendChild(newDiv);
 
-    // Clear input fields
     usernameInput.value = '';
     taskInput.value = '';
 }
-
-const addButton = document.getElementById('submit');
 
 if (addButton) {
     addButton.addEventListener('click', () => {
