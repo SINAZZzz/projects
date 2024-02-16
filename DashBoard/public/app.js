ec1bed52ed391f25d2cfa56d512774fa618d2ccd
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-02-16 23:41:32 +0330

    new commit add dashboard style tailwind config and html and ts

---
+++ DashBoard/public/app.js
@@ -0,0 +1,33 @@
+"use strict";
+var _a;
+(_a = document.getElementById('sidebar-toggle')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+    var sidebar = document.querySelector('.sidebar');
+    if (sidebar) {
+        sidebar.classList.toggle('-translate-x-full');
+    }
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.nav-link');
+    var contents = document.querySelectorAll('.content');
+    // Hide all content except the default one
+    contents.forEach(function (content) {
+        if (!content.classList.contains('block')) {
+            content.style.display = 'none';
+        }
+    });
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var target = this.getAttribute('data-target');
+            contents.forEach(function (content) {
+                content.style.display = 'none';
+            });
+            if (target) {
+                var targetContent = document.getElementById(target + '-content');
+                if (targetContent) {
+                    targetContent.style.display = 'block';
+                }
+            }
+        });
+    });
+});
---
+++ DashBoard/src/app.ts
@@ -0,0 +1,36 @@
+document.getElementById('sidebar-toggle')?.addEventListener('click', () => {
+    const sidebar = document.querySelector<HTMLElement>('.sidebar');
+    if (sidebar) {
+        sidebar.classList.toggle('-translate-x-full');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('.nav-link');
+    const contents: NodeListOf<HTMLElement> = document.querySelectorAll('.content');
+
+    // Hide all content except the default one
+    contents.forEach(function (content) {
+        if (!content.classList.contains('block')) {
+            content.style.display = 'none';
+        }
+    });
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const target: string | null = this.getAttribute('data-target');
+
+            contents.forEach(function (content) {
+                content.style.display = 'none';
+            });
+
+            if (target) {
+                const targetContent: HTMLElement | null = document.getElementById(target + '-content');
+                if (targetContent) {
+                    targetContent.style.display = 'block';
+                }
+            }
+        });
+    });
+});
